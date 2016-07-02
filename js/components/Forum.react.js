498a748a4f7254f614228a616df248842108a3bd
Yuriy Shefer <sheff@sheffmachine.com>
2016-07-01 19:33:48 -0700

    set up the basic react structure and add in the navbar with pure JS without the use of a helper

---
+++ js/components/Forum.react.js
@@ -0,0 +1,9 @@
+var Forum = React.createClass({
+  render: function () {
+    return React.createElement(
+      'div', //div element
+       null, //no attributes
+      React.createElement(ForumHeader, null)
+     );
+  }
+});
