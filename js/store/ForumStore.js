348c07a299f0a71cfeae4d598db31a69d870472f
Yuriy Shefer <sheff@sheffmachine.com>
2016-07-10 19:24:49 -0700

    refactor the code to add string constants and actions

--- js/store/ForumStore.js
+++ js/store/ForumStore.js
@@ -45,12 +45,12 @@ ForumStore.markAsCorrect = function (id) {
 
 ForumDispatcher.register(function (action) {
   switch (action.actionType) {
-    case 'FORUM_ANSWER_ADDED': {
+    case ForumConstants.FORUM_ANSWER_ADDED: {
       console.log('Answer added');
-      ForumStore.addData(action.data);
+      ForumStore.addData(action.newAnswer);
       break;
     }
-    case 'FORUM_ANSWER_MARKED_CORRECT': {
+    case ForumConstants.FORUM_ANSWER_MARKED_CORRECT: {
       console.log('answer marked correct');
       ForumStore.markAsCorrect(action.id);
       break;
